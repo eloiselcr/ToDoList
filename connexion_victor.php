002c5d4e6d5292babbb62598ad3529ebe620ea70
eloiselcr <eloise.lecronier@gmail.com>
2023-02-08 17:26:42 +0100

    Commit ajout revu connexion

--- connexion_victor.php
+++ connexion_victor.php
@@ -10,11 +10,82 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>APPLICATION PHP BDD - CONSULTATION</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
+
 </head>
+<style>
+      body{
+  display: flex;
+  justify-content:center;
+  flex-direction: columns;
+  align-items: center;
+  background-color: #f5f5f5;
+  font-family: 'Roboto', sans-serif;
+}
+form {
+  margin-top: 20px;
+  background-color: #fff;
+  padding: 40px 60px;
+  border-radius: 10px;
+  min-width: 300px;
+}
+form h1{
+  color: #eb7371;
+  text-align:center;
+}
+form .social-media{
+  margin-top: -10px;
+  display: flex;
+  flex-wrap:wrap;
+  justify-content:center;
+}
+form .social-media p{
+  padding: 5px;
+  width: 20px;
+  margin-left: 10px;
+  border-radius: 100%;
+  border: 1px solid #545454;
+  text-align: center;
+  cursor:pointer;
+  color: #545454;
+}
+form p.choose-email{
+  text-align:center;
+}
+form .inputs {
+  display: flex;
+  flex-direction: column;
+}
+form .inputs input[type='text'], input[type='password']{
+  padding: 15px;
+  border:none;
+  border-radius: 5px;
+  background-color:#f2f2f2;
+  outline:none;
+  margin-bottom: 15px;
+}
+form p.inscription{
+  font-size: 14px;
+  margin-bottom: 20px;
+  color: #878787;
+}
+form p.inscription span{
+  color: #eb7371;
+}
+form button{
+  padding: 15px 25px;
+  border-radius: 5px;
+  border:none;
+  font-size: 15px;
+  color: #fff;
+  background-color: #eb7371;
+  outline:none;
+  cursor:pointer;
+}
+</style>
 
 <body>
 
-    <h1> Bienvenue sur votre page d'accès ToDoList </h1>
 
     <?php
 
@@ -38,26 +109,53 @@ session_start();
             $_SESSION["toto"] = "titi";
         }
     }
+    if(isset($_POST['deconnexion'])) // SESSION DESTROY pour la Deconnexion
+    {
+        session_destroy();
+        header('Location: connexion_victor.php');
+    }
 
     
     if (isset($_SESSION["toto"]) && $_SESSION["toto"]=="titi") {
-        echo "vous etes connexté";
-
+        echo 'vous etes connexté
+        <form method="post">
+        <input type="submit" value="deconnexion" name="deconnexion">
+        </form>';
+        
     } else {
 
 
     ?>
 
 
-        <form action="" method="post">
-            <label for="username">Entrez votre nom d'utilisateur :</label>
-            <input type="text" id="username" name="username">
+<form method="post" >     
+        <h1>Se connecter</h1>
+        <div class="social-media">
+          <p><i class="fab fa-google"></i></p>
+          <p><i class="fab fa-youtube"></i></p>
+          <p><i class="fab fa-facebook-f"></i></p>
+          <p><i class="fab fa-twitter"></i></p>
+        </div>
+        <p class="choose-email">ou utiliser mon adresse e-mail :</p>
+        
+        <div class="inputs">
+          <input type="text" id="username" name="username" placeholder="Uset">
+          <input type="password" id="password" name="password" placeholder="mdp">
+        </div>
+        
+        <p class="inscription">Je n'ai pas de <span>compte</span>. Je m'en <a href="">créer</a><span>
+                        
+                 
+          <script>
+            document.getElementById("show-form").addEventListener("click", function() {
+              document.getElementById("form-container").style.display = "block";
+            });
+          </script></span> un.</p>
+        <div align="center">
+          <button type="submit" name="connexion" >Se connecter</button>
+        </div>
+      </form>
 
-            <label for="password">Entrez votre mot de passe :</label>
-            <input type="password" id="password" name="password">
-
-            <input type="submit" value="Submit" name="connexion">
-        </form>
 
     <?php } ?>
 
---
+++ fichiereloise/connexion.php
@@ -0,0 +1,203 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>APPLICATION PHP BDD - TODOLIST</title>
+    <link rel="stylesheet" href="connexion_css.css">
+    
+</head>
+
+<body>
+
+    <h1> Bienvenue sur votre page d'accès ToDoList </h1>
+
+    <?php
+
+    function afficher_formulaire() // Affichage du formulaire
+    {
+    ?>
+        <form action="" method="post">
+            <label for="username">Entrez votre nom d'utilisateur :</label>
+            <input type="text" id="username" name="username">
+            </br>
+            </br>
+            <label for="password">Entrez votre mot de passe :</label>
+            <input type="password" id="password" name="password">
+
+            <input type="submit" value="Submit" name="connexion">
+        </form>
+    <?php
+    }
+
+    try { // Connexion à la base de donnée 
+        $ipserver = "192.168.64.86";
+        $nomBase = "ToDoList";
+        $loginPrivilege = "Utilisateur";
+        $passPrivilege = "todolist1234";
+
+        $GLOBALS["pdo"] = new PDO('mysql:host=' . $ipserver . ';dbname=' . $nomBase . '', $loginPrivilege, $passPrivilege);
+    } catch (Exception $error) {
+        echo "error est : " . $error->getMessage();
+    }
+
+
+    if (isset($_POST["connexion"])) // Vérification des login pour un compte après avoir appuyé sur le bouton "Connexion"
+    {
+        $requete = "SELECT * FROM `Utilisateurs` WHERE Utilisateurs.login = '" . $_POST["username"] . "' AND Utilisateurs.mdp='" . $_POST["password"] . "';";
+        $resultat = $GLOBALS["pdo"]->query($requete);
+        if ($resultat->rowCount() > 0) {
+
+            $_SESSION["trueconnect"] = "true";
+        }
+    }
+
+
+    if (isset($_SESSION["trueconnect"]) && $_SESSION["trueconnect"] == "true") 
+    {
+        echo "vous etes connecté";
+        ?>
+
+
+<section class="vh-100 gradient-custom-2">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-12 col-xl-10">
+
+        <div class="card mask-custom">
+          <div class="card-body p-4 text-white">
+
+            <div class="text-center pt-3 pb-2">
+              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-todo-list/check1.webp"
+                alt="Check" width="60">
+              <h2 class="my-4">Task List</h2>
+            </div>
+
+            <table class="table text-white mb-0">
+              <thead>
+                <tr>
+                  <th scope="col">Team Member</th>
+                  <th scope="col">Task</th>
+                  <th scope="col">Priority</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="fw-normal">
+                  <th>
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                      alt="avatar 1" style="width: 45px; height: auto;">
+                    <span class="ms-2">Alice Mayer</span>
+                  </th>
+                  <td class="align-middle">
+                    <span>Call Sam For payments</span>
+                  </td>
+                  <td class="align-middle">
+                    <h6 class="mb-0"><span class="badge bg-danger">High priority</span></h6>
+                  </td>
+                  <td class="align-middle">
+                    <a href="#!" data-mdb-toggle="tooltip" title="Done"><i
+                        class="fas fa-check fa-lg text-success me-3"></i></a>
+                    <a href="#!" data-mdb-toggle="tooltip" title="Remove"><i
+                        class="fas fa-trash-alt fa-lg text-warning"></i></a>
+                  </td>
+                </tr>
+                <tr class="fw-normal">
+                  <th>
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
+                      alt="avatar 1" style="width: 45px; height: auto;">
+                    <span class="ms-2">Kate Moss</span>
+                  </th>
+                  <td class="align-middle">Make payment to Bluedart</td>
+                  <td class="align-middle">
+                    <h6 class="mb-0"><span class="badge bg-success">Low priority</span></h6>
+                  </td>
+                  <td class="align-middle">
+                    <a href="#!" data-mdb-toggle="tooltip" title="Done"><i
+                        class="fas fa-check fa-lg text-success me-3"></i></a>
+                    <a href="#!" data-mdb-toggle="tooltip" title="Remove"><i
+                        class="fas fa-trash-alt fa-lg text-warning"></i></a>
+                  </td>
+                </tr>
+                <tr class="fw-normal">
+                  <th>
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3-bg.webp"
+                      alt="avatar 1" style="width: 45px; height: auto;">
+                    <span class="ms-2">Danny McChain</span>
+                  </th>
+                  <td class="align-middle">Office rent</td>
+                  <td class="align-middle">
+                    <h6 class="mb-0"><span class="badge bg-warning">Middle priority</span></h6>
+                  </td>
+                  <td class="align-middle">
+                    <a href="#!" data-mdb-toggle="tooltip" title="Done"><i
+                        class="fas fa-check fa-lg text-success me-3"></i></a>
+                    <a href="#!" data-mdb-toggle="tooltip" title="Remove"><i
+                        class="fas fa-trash-alt fa-lg text-warning"></i></a>
+                  </td>
+                </tr>
+                <tr class="fw-normal">
+                  <th>
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava5-bg.webp"
+                      alt="avatar 1" style="width: 45px; height: auto;">
+                    <span class="ms-2">Alexa Chung</span>
+                  </th>
+                  <td class="align-middle">Office grocery shopping</td>
+                  <td class="align-middle">
+                    <h6 class="mb-0"><span class="badge bg-danger">High priority</span></h6>
+                  </td>
+                  <td class="align-middle">
+                    <a href="#!" data-mdb-toggle="tooltip" title="Done"><i
+                        class="fas fa-check fa-lg text-success me-3"></i></a>
+                    <a href="#!" data-mdb-toggle="tooltip" title="Remove"><i
+                        class="fas fa-trash-alt fa-lg text-warning"></i></a>
+                  </td>
+                </tr>
+                <tr class="fw-normal">
+                  <th class="border-0">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp"
+                      alt="avatar 1" style="width: 45px; height: auto;">
+                    <span class="ms-2">Ben Smith</span>
+                  </th>
+                  <td class="border-0 align-middle">Ask for Lunch to Clients</td>
+                  <td class="border-0 align-middle">
+                    <h6 class="mb-0"><span class="badge bg-success">Low priority</span></h6>
+                  </td>
+                  <td class="border-0 align-middle">
+                    <a href="#!" data-mdb-toggle="tooltip" title="Done"><i
+                        class="fas fa-check fa-lg text-success me-3"></i></a>
+                    <a href="#!" data-mdb-toggle="tooltip" title="Remove"><i
+                        class="fas fa-trash-alt fa-lg text-warning"></i></a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php
+    } 
+    else 
+    {
+        afficher_formulaire();
+    }
+
+
+?>
+
+
+</body>
+</html>
