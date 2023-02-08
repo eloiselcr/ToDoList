730fea5b031698b72193cf8a10418c67e94c4f69
eloiselcr <eloise.lecronier@gmail.com>
2023-02-08 17:26:50 +0100

    oubli

--- fichiereloise/connexion.php
+++ fichiereloise/connexion.php
@@ -10,7 +10,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>APPLICATION PHP BDD - TODOLIST</title>
-    <link rel="stylesheet" href="connexion_css.css">
+    <link rel="stylesheet" href="connexion.css">
     
 </head>
 
